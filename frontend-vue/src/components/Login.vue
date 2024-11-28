5ef2746f55bec28c40fc21601422849874cfbac7
daffalandra <mdaf.alandra@gmail.com>
2024-11-28 11:58:43 +0700

    Feat: Login

--- frontend-vue/src/components/Login.vue
+++ frontend-vue/src/components/Login.vue
@@ -1,28 +1,25 @@
 <template>
-    <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                        v-model="form.email">
-                </div>
+    <head>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    </head>
     <div class="container">
         <div class="col-md-6 m-auto">
             <h1>Login page</h1>
             <form @submit.prevent="submitLogin">
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                        v-model="form.email">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" v-model="form.password">
+                    <input type="password" class="form-control" id="exampleInputPassword1">
                 </div>
-                <button type="submit" class="btn btn-primary">Login</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
 </template>
-<!-- <script setup>
+<script setup>
 import { ref } from 'vue';
 import axios from 'axios';
 
@@ -44,4 +41,4 @@ const submitLogin = async () => {
     router.push('/');
 }
 
-</script> -->
+</script>
